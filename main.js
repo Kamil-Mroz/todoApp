1fdc3d35b93ebae656145ef3f238b91c67192b99
Kamil Mróz <kamilmp17@gmail.com>
2022-08-20 10:32:37 +0200

    added notifications

--- main.js
+++ main.js
@@ -5,27 +5,20 @@ const btnDelete = document.getElementById("delete");
 const colorMode = document.querySelector(".color-mode");
 const list = document.querySelector(".todo__list");
 const formEl = document.getElementById("form");
+const body = document.body;
+const notificationsEl = document.querySelector(".notifications");
 
 let dane;
 let todo = [];
 init();
+
 function init() {
   dane = getLocalStorage();
+
   if (!dane) return;
   todo = dane;
   todo.forEach((element) => {
-    const text = `
-      <li class="todo__item ">
-        <span class="todo__text">${element}</span>
-        <button class="btn btn--done" id="done">
-            <ion-icon name="checkmark-circle-outline"></ion-icon>
-        </button>
-        <button class="btn btn--delete" id="delete">
-            <ion-icon name="trash-outline" class="icon"></ion-icon>
-        </button>
-        </li>
-    `;
-    list.insertAdjacentHTML("afterend", text);
+    render(element);
   });
 }
 
@@ -33,24 +26,15 @@ formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
   if (!inputEl.value) {
-    return console.log("Please enter a valid input");
+    return displayNotifications("error");
   }
   const data = inputEl.value;
 
   inputEl.value = "";
 
-  const text = `
-  <li class="todo__item ">
-  <span class="todo__text">${data}</span>
-  <button class="btn btn--done" id="done">
-  <ion-icon name="checkmark-circle-outline"></ion-icon>
-  </button>
-  <button class="btn btn--delete" id="delete">
-  <ion-icon name="trash-outline" class="icon"></ion-icon>
-  </button>
-  </li>
-  `;
-  list.insertAdjacentHTML("afterend", text);
+  render(data);
+
+  displayNotifications("added");
 
   todo.push(data);
 
@@ -68,3 +52,37 @@ function getLocalStorage() {
 colorMode.addEventListener("click", function () {
   document.body.classList.toggle("light");
 });
+
+function displayNotifications(action) {
+  const text = {
+    added: "Added activity",
+    done: "Finished successfully",
+    error: "Enter an input value",
+  };
+
+  const notify = document.createElement("div");
+  const message =
+    action === "added" ? "added" : action === "error" ? "error" : action;
+
+  notify.classList.add(action);
+  notify.innerText = text[message];
+  notificationsEl.appendChild(notify);
+
+  setTimeout(() => {
+    notify.remove();
+  }, 4000);
+}
+
+function render(data) {
+  const text = `
+    <li class="todo__item ">
+    <span class="todo__text">${data}</span>
+    <button class="btn btn--done" id="done">
+    <ion-icon name="checkmark-circle-outline"></ion-icon>
+    </button>
+    <button class="btn btn--delete" id="delete">
+    <ion-icon name="trash-outline" class="icon"></ion-icon>
+    </button>
+    </li>`;
+  list.insertAdjacentHTML("afterbegin", text);
+}
